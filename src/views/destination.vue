7bda4164006ac847941280cac7f8addf75944a7a
euheber <euheber1@gmail.com>
2023-01-23 15:24:08 -0300

    add: handleActiveClass in btn

--- src/views/destination.vue
+++ src/views/destination.vue
@@ -5,35 +5,34 @@
   <main>
     <section id="chosen-planet">
       <h1><span>01</span> Pick your destination</h1>
-      <img src="/destination/image-moon.png" alt="Moon picture" loading="lazy">
+      <img :src="planets.imgDir" alt="Moon picture" loading="lazy">
     </section>
 
     <section id="choose-destination">
       <ul>
-        <li>Moon</li>
-        <li>Mars</li>
-        <li>Europa</li>
-        <li>Titan</li>
+        <li @click="handleActiveClass = 'moon'" :class="[handleActiveClass === 'moon' ? 'active' : '']">Moon</li>
+        <li @click="handleActiveClass = 'mars'" :class="[handleActiveClass === 'mars' ? 'active' : '']">Mars</li>
+        <li @click="handleActiveClass = 'europa'" :class="[handleActiveClass === 'europa' ? 'active' : '']">Europa</li>
+        <li @click="handleActiveClass = 'titan'" :class="[handleActiveClass === 'titan' ? 'active' : '']">Titan</li>
       </ul>
 
       <h1>
-        moon
+        {{ planets.planet }}
       </h1>
 
       <p class="planet-info">
-        See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come
-        back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+        {{ planets.content }}
       </p>
 
       <div id="travel-info-footer">
         <div id="distance">
           <p>AVG. Distance</p>
-          <h2>384,400 km</h2>
+          <h2> {{ planets.distance }}</h2>
         </div>
 
         <div id="travel-time">
           <p>Est. travel time</p>
-          <h2>3 days</h2>
+          <h2>{{ planets.travelTime }}</h2>
         </div>
       </div>
     </section>
@@ -42,12 +41,15 @@
 
 <script setup>
 import navigation from '../components/navigation/navigation.vue';
+import { ref } from 'vue';
+
+let handleActiveClass = ref('moon');
 
 
 let planets = {
-  planet: 'MOON',
+  planet: 'moon',
   content: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
-  imgDir: '',
+  imgDir: '/destination/image-moon.png',
   distance: '384,400 km',
   travelTime: '3 days'
 }
