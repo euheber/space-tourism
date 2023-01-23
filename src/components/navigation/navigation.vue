11b1b79afc27785a65e98fbf12b0842d3b7767e7
euheber <euheber1@gmail.com>
2023-01-23 14:59:18 -0300

    add: destination view

--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -3,24 +3,36 @@
         <img src="/shared/logo.svg" alt="Space Travel logo">
 
         <ul>
-            <li>
-                <router-link to="/">
+            <li :class="[currentPathname === 'home' ? 'active' : '']">
+                <router-link to="/" class="clicable-area">
                     <span>00</span> Home
                 </router-link>
             </li>
-            <li>
-                <router-link to="/destination">
+            <li :class="[currentPathname === 'destination' ? 'active' : '']">
+                <router-link to="/destination" class="clicable-area">
                     <span>01</span> Destination
                 </router-link>
             </li>
-            <li><span>02</span> Crew</li>
-            <li><span>03</span> Technology</li>
+            <li> <router-link to="/destination" class="clicable-area">
+                    <span>02</span> Crew
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/destination" class="clicable-area">
+                    <span>03</span> Technology
+                </router-link>
+            </li>
         </ul>
     </nav>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+let currentPathname = router.currentRoute.value.name;
 
 </script>
 <style lang="sass" scoped src="../../assets/scss/navigation/navigation.scss" />
--- src/views/destination.vue
+++ src/views/destination.vue
@@ -1,14 +1,61 @@
 <template>
-  <navigation />
+  <header>
+    <navigation />
+  </header>
+  <main>
+    <section id="chosen-planet">
+      <h1><span>01</span> Pick your destination</h1>
+      <img src="/destination/image-moon.png" alt="Moon picture" loading="lazy">
+    </section>
+
+    <section id="choose-destination">
+      <ul>
+        <li>Moon</li>
+        <li>Mars</li>
+        <li>Europa</li>
+        <li>Titan</li>
+      </ul>
+
+      <h1>
+        moon
+      </h1>
+
+      <p class="planet-info">
+        See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come
+        back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+      </p>
+
+      <div id="travel-info-footer">
+        <div id="distance">
+          <p>AVG. Distance</p>
+          <h2>384,400 km</h2>
+        </div>
+
+        <div id="travel-time">
+          <p>Est. travel time</p>
+          <h2>3 days</h2>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script setup>
 import navigation from '../components/navigation/navigation.vue';
+
+
+let planets = {
+  planet: 'MOON',
+  content: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+  imgDir: '',
+  distance: '384,400 km',
+  travelTime: '3 days'
+}
 
 const body = document.querySelector('body')
 body.style.background = 'url(destination/background-destination-desktop.jpg) no-repeat center'
 body.style.backgroundSize = 'cover'
 </script>
 
-<style  lang="sass" scoped />
+<style  lang="sass" scoped src="../assets/scss/destinationView/destinationView.scss" />
 
