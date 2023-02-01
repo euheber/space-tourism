3c301ec4265e02484d608686ab012fa85bf79654
euheber <euheber1@gmail.com>
2023-02-01 13:05:41 -0300

    fixed:body scroll on opening menu

--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -26,33 +26,32 @@
         </ul>
 
         <button id="mobile-menu-btn">
-            <img src="/shared/icon-hamburger.svg" alt="Abrir menu" v-if="handleBtn" @click="handleMobileMenu" >
-            <img src="/shared/icon-close.svg" alt="Fechar menu" v-else @click="handleMobileMenu" >
+            <img src="/shared/icon-hamburger.svg" alt="Abrir menu" v-if="handleBtn" @click="handleMobileMenu">
+            <img src="/shared/icon-close.svg" alt="Fechar menu" v-else @click="handleMobileMenu">
         </button>
 
         <ul id="mobile-navigation" v-if="mobileMenu">
             <li :class="[currentPathname === 'home' ? 'active' : '']">
-                <router-link to="/" class="mobile-clicable-area">
+                <router-link to="/" class="mobile-clicable-area" @click="handleMobileMenu">
                     <span>00</span> Home
                 </router-link>
             </li>
             <li :class="[currentPathname === 'destination' ? 'active' : '']">
-                <router-link to="/destination" class="mobile-clicable-area">
+                <router-link to="/destination" class="mobile-clicable-area" @click="handleMobileMenu">
                     <span>01</span> Destination
                 </router-link>
             </li>
             <li :class="[currentPathname === 'crew' ? 'active' : '']">
-                <router-link to="/crew" class="mobile-clicable-area">
+                <router-link to="/crew" class="mobile-clicable-area" @click="handleMobileMenu">
                     <span>02</span> Crew
                 </router-link>
             </li>
             <li :class="[currentPathname === 'technology' ? 'active' : '']">
-                <router-link to="/technology" class="mobile-clicable-area">
+                <router-link to="/technology" class="mobile-clicable-area" @click="handleMobileMenu">
                     <span>03</span> Technology
                 </router-link>
             </li>
         </ul>
-
     </nav>
 </template>
 
@@ -61,17 +60,25 @@ import { RouterLink } from 'vue-router';
 import { useRoute } from 'vue-router'
 import { watchEffect } from 'vue'
 import { ref } from 'vue';
+
+const body = document.querySelector('body')
 const router = useRoute()
-
 let currentPathname = ref('home')
-
 let handleBtn = ref(true)
 let mobileMenu = ref(false)
 
-const handleMobileMenu = () => { 
+
+const handleMobileMenu = () => {
     handleBtn.value = !handleBtn.value
     mobileMenu.value = !mobileMenu.value
+
+    if (mobileMenu.value) {
+        body.style.overflow = 'hidden'
+    } else {
+        body.style.overflow = 'scroll '
+    }
 }
+
 watchEffect(() => {
     currentPathname.value = router.name
 })
